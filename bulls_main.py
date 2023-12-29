876411d67f444d069cafa6d958496b362c17b6f0
sweetmjkim <sweet.mjkim@gmail.com>
2023-12-29 14:55:38 +0900

    commit

--- bulls_main.py
+++ bulls_main.py
@@ -1,5 +1,5 @@
-import _29cm_subfunction_js as subfunction_js
-import _29cm_subfunction_mj as subfunction_mj
+import bulls_subfunction_js as subfunction_js
+import bulls_subfunction_mj as subfunction_mj
 # 기본 function 형식 - 기다림. 불리울 때 기능한다. def function () : -> tap pass -> return 0
 def main() :
     try:   # 업무 코드
